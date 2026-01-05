2230767fe71e5c7b2ab1394fef31eb38447e19c1
Gee <alickoamarko@gmail.com>
2026-01-05 11:01:28 +0100

    Fixed duplication

--- map-editor-base/editor.gd
+++ map-editor-base/editor.gd
@@ -246,6 +246,7 @@ func _input(event: InputEvent) -> void:
 			KEY_D:
 				if Input.is_key_pressed(KEY_ALT) and selected_map_object:
 					var clone := selected_map_object.duplicate()
+					clone.data = selected_map_object.data
 					add_child(clone)
 					SelectMapObject(clone)
 
